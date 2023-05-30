8d6fde95ac1c274610c6379284ab49b07846cd78
Ali Akbar <aliakbarboohar@gmail.com>
2023-05-30 18:56:49 +0500

    changed the some codes

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -20,9 +20,9 @@ export const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const url = "http://localhost:8080/api/users";
-      const { data: res } = await axios.post(url, data);
-      console.log(res.message); // Optional: Log the response message
+      const url = "/register"; // Update the endpoint URL
+      const response = await axios.post(url, data); // Send the data to the server
+      console.log(response.data.message); // Optional: Log the response message
       navigate("/login"); // Navigate to the login page
     } catch (error) {
       if (
@@ -31,9 +31,15 @@ export const Register = () => {
         error.response.status <= 500
       ) {
         setError(error.response.data.message);
+        console.log(error.response.status);
+      }
+      if (error.response.status === 201) {
+        alert("User registration done");
+        setData({ ...data,firstName: "", lastName: "", email: "", password: "" });
       }
     }
   };
+  
 
   return (
     <div className="signup_container">
@@ -41,54 +47,17 @@ export const Register = () => {
         <div className="left">
           <h1>Welcome Back</h1>
           <Link to="/login">
-            <button type="button" className="white_btn">
-              Sign in
-            </button>
+            <button type="button" className="white_btn"> Sign in</button>
           </Link>
         </div>
         <div className="right">
           <form className="form_container" onSubmit={handleSubmit}>
             <h1>Create Account</h1>
-            <input
-              type="text"
-              placeholder="First Name"
-              name="firstName"
-              onChange={handleChange}
-              value={data.firstName}
-              required
-              className="input"
-            />
-            <input
-              type="text"
-              placeholder="Last Name"
-              name="lastName"
-              onChange={handleChange}
-              value={data.lastName}
-              required
-              className="input"
-            />
-            <input
-              type="email"
-              placeholder="Email"
-              name="email"
-              onChange={handleChange}
-              value={data.email}
-              required
-              className="input"
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              name="password"
-              onChange={handleChange}
-              value={data.password}
-              required
-              className="input"
-            />
-            {error && <div className="error_msg">{error}</div>}
-            <button type="submit" className="green_btn">
-              Sign Up
-            </button>
+            <input type="text"  placeholder="First Name"  name="firstName"  onChange={handleChange}  value={data.firstName}  required  className="input"/>
+            <input type="text" placeholder="Last Name" name="lastName"  onChange={handleChange}  value={data.lastName}  required  className="input" />
+            <input type="email"  placeholder="Email"  name="email"  onChange={handleChange}  value={data.email}  required className="input"/>
+            <input type="password" placeholder="Password" name="password" onChange={handleChange} value={data.password} required className="input"/>
+            <button type="submit" className="green_btn"> Sign Up </button>
           </form>
         </div>
       </div>
